b472ac7b94430be75f7759a1bc6ea623cc2c7e28
mwblevins <michaelwblevins@gmail.com>
2022-09-16 13:45:17 -0700

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Success
+ *
+ * Return: Always 0
+ *
+ */
+
+void main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
