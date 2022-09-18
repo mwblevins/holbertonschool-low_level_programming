c71a236b85018d4afe3b5d68293e8aed3767abf6
mwblevins <michaelwblevins@gmail.com>
2022-09-17 18:23:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Success
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
