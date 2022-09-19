1228db50087bb274918e54299bb04689906d7617
mwblevins <michaelwblevins@gmail.com>
2022-09-19 10:56:40 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+	return (0);
+}
