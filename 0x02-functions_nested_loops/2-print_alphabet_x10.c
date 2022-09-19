a99371acb7779b13b6fdcbed9282d61324606a2c
mwblevins <michaelwblevins@gmail.com>
2022-09-19 13:18:11 -0700

    Update head

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (a = 0; a <= 9; a++)
 {
-		for (b = 97; b <= 127; b++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 
 int _putchar(char);
 void print_alphabet(void);
-void print_alphabet(void);
+void print_alphabetx10(void);
