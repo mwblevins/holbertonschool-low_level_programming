c2d96e3595152657539f9faf1031b9290cdcc4d8
mwblevins <michaelwblevins@gmail.com>
2022-09-20 07:46:43 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ *@n: random int
+ * Return: value of the last digit
+ *
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
