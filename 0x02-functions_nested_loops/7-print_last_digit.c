42ebec5dc4fbef05571670be00a3986027fd43f9
mwblevins <michaelwblevins@gmail.com>
2022-09-20 07:51:40 -0700

    Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,5 +10,17 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int i;
+
+	i = n % 10;
+	if (i < 0)
+	{
+		_putchar(-i + 48);
+		return (-i);
+	}
+	else
+	{
+		_putchar(i + 48);
+		return (i);
+	}
 }
