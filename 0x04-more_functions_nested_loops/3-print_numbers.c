acc09bdf7a454ed9f30fe2b06bdc3e0d588e0c39
mwblevins <michaelwblevins@gmail.com>
2022-09-22 13:56:05 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks the code
+ *
+ * Return: null
+ */
+
+void print_numbers(void)
+
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
