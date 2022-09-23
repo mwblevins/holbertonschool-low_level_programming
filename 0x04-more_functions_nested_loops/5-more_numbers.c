a610dc42fa4058f55ebc7db423d3a0c4a9610471
mwblevins <michaelwblevins@gmail.com>
2022-09-23 08:32:46 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - checks the code
+ *
+ * Return: null
+ */
+
+void more_numbers(void)
+
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
