6a2915905eb3d82fef7141fd9ab8c6123ef637fa
mwblevins <michaelwblevins@gmail.com>
2022-09-23 09:03:56 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 	for (a = 2; a <= 100; a++)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (a % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (a % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else
 			printf("%d", a);
 	}
