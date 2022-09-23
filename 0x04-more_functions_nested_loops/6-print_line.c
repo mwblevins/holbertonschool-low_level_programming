1e01335f603041a808786a72dade668b255cc0ea
mwblevins <michaelwblevins@gmail.com>
2022-09-23 08:40:10 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - checks the code
+ *@n: number of times the line is printed
+ * Return: null
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
