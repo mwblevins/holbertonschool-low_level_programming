c39fefbf429ca055c8aadcdfc105d73297b506fe
mwblevins <michaelwblevins@gmail.com>
2022-09-26 08:37:22 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - Checks the code
+ *
+ * @a:input a
+ * @b: input b
+ *
+ * Return: void
+ */
+
+void swap_int(int *a,  int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
