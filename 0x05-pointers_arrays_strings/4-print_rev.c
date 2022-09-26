057804cf06a5d271d40fac8b925d5a61eef4a191
mwblevins <michaelwblevins@gmail.com>
2022-09-26 09:55:53 -0700

    Print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,18 @@
 
 void print_rev(char *s)
 {
-	if (*s != '\0')
+	int i = 0;
+
+	while (i >= 0)
 	{
-		print_rev(s + 1);
-		_putchar(*s);
+		if (s[i] == '\0')
+			break;
+		i++;
 	}
-	else
-	{
-		_putchar('\n');
-	}
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
 
 
