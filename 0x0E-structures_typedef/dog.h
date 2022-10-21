ba02475b8922438f9d801db26cc9ac5f1eb124cd
mwblevins <michaelwblevins@gmail.com>
2022-10-21 11:23:49 -0700

    doge

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: struct called dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
