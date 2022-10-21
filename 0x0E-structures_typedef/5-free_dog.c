a0cab550140f8164316425c52717279d66803e5e
mwblevins <michaelwblevins@gmail.com>
2022-10-21 11:58:24 -0700

    be free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees the doges
+ * @d: struct dog
+ *
+ * Return: none
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
