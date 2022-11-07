bdbd2a27f0375de6ee6374ec8bf9ddf89295ae81
mwblevins <michaelwblevins@gmail.com>
2022-11-07 09:07:08 -0800

    list int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints the elements
+ * @h: linked list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_t num;
+
+	num = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
